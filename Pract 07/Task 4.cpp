9e6a006ffc5e9fa845883ca2a8c3d422fc9ace8f
gdemirova <gergana.demirova04@gmail.com>
2024-11-19 14:28:14 +0200

    Update Task 4.cpp

--- Pract 07/Task 4.cpp
+++ Pract 07/Task 4.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 
 int powerOfTen(int power) {
-    int result = 1;
-    for (int i = 1; i < power; i++)
-    {
-        result *= 10;
-    }
-    return result;
+        int result = 1;
+        for (int i = 1; i < power; i++) {
+            result *= 10;
+        }
+        return result;
 }
 
 int getDigit(int num, int k) {
-    int position = powerOfTen(k - 1);
-    return (num / position) % 10;
+        int position = powerOfTen(k - 1);
+        return (num / position) % 10;
 }
 
 int setDigit(int num, int k, int newDigit) {
-    int position = powerOfTen(k - 1);
-    int currentDigit = (num / position) % 10;
-    return num - currentDigit * position + newDigit * position;
+        int position = powerOfTen(k - 1);
+        int currentDigit = (num / position) % 10;
+        return num - currentDigit * position + newDigit * position;
 }
 
 void swapKthDigits(int m, int n, int k) {
-    int mKthDigit = getDigit(m, k);
-    int nKthDigit = getDigit(n, k);
-
-    int newM = setDigit(m, k, nKthDigit);
-    int newN = setDigit(n, k, mKthDigit);
-  
-    std::cout << "New m = " << newM << std::endl << "New n = " << n;
+        int mKthDigit = getDigit(m, k);
+        int nKthDigit = getDigit(n, k);
+    
+        int newM = setDigit(m, k, nKthDigit);
+        int newN = setDigit(n, k, mKthDigit);
+      
+        std::cout << "New m = " << newM << std::endl << "New n = " << n;
 }
 
 int main()
 {
-    int m, n, k;
-    std::cin >> m >> n >> k;
-
-    swapKthDigits(m, n, k);
+        int m, n, k;
+        std::cin >> m >> n >> k;
+    
+        swapKthDigits(m, n, k);
 }
