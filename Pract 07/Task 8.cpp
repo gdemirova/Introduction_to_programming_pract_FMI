a5d26610b929543fab4df916f81f59925228bdc0
gdemirova <gergana.demirova04@gmail.com>
2024-11-19 14:45:42 +0200

    Update Task 8.cpp

--- Pract 07/Task 8.cpp
+++ Pract 07/Task 8.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 
 int sumDigits(int n) {
-      	int sum = 0;
-      	while (n > 0) {
-        	int lastDigit = n % 10;
+      int sum = 0;
+      while (n > 0) {
+      	int lastDigit = n % 10;
         	sum += lastDigit;
         	n /= 10;
-      	}
+      }
       
-      	return sum;
+      return sum;
 }
 
 int maxSum(int n) {
-      	int maxSumDigits = 0;
-      	int maxNumber = 0;
-      	int userInput;
+      int maxSumDigits = 0;
+      int maxNumber = 0;
+      int userInput;
       
-      	for (int i = 0; i < n; i++) {
-          		std::cin >> userInput;
-          		int currentSumDigits = sumDigits(userInput);
+      for (int i = 0; i < n; i++) {
+         	std::cin >> userInput;
+          	int currentSumDigits = sumDigits(userInput);
           
-          		if (currentSumDigits > maxSumDigits) {
-              		maxSumDigits = currentSumDigits;
+          	if (currentSumDigits > maxSumDigits) {
+             		maxSumDigits = currentSumDigits;
               		maxNumber = userInput;
-          		}
-      	}
-      	return maxNumber;
+          	}
+      }
+	return maxNumber;
 }
 
 int main() 
 {
-      	int n;
-      	std::cin >> n;
-      	std::cout << maxSum(n);
+      int n;
+      std::cin >> n;
+      std::cout << maxSum(n);
 }
