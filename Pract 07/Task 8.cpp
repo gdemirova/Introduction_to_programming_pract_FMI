2f22a38378312649bf6c617b35bd753c783e17d1
gdemirova <gergana.demirova04@gmail.com>
2024-11-19 14:43:59 +0200

    Update Task 8.cpp

--- Pract 07/Task 8.cpp
+++ Pract 07/Task 8.cpp
@@ -3,9 +3,9 @@
 int sumDigits(int n) {
       	int sum = 0;
       	while (n > 0) {
-        		int lastDigit = n % 10;
-        		sum += lastDigit;
-        		n /= 10;
+        	int lastDigit = n % 10;
+        	sum += lastDigit;
+        	n /= 10;
       	}
       
       	return sum;
